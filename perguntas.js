29d07a1705899597d9901e25307bedb96222087a
20-moraesweslei <140934241+20-moraesweslei@users.noreply.github.com>
2025-09-18 05:21:53 +0000

    1

--- perguntas.js
+++ perguntas.js
@@ -2,4 +2,10 @@ criaCartao(
     'Música',
     'Quem é conhecido como o "Rei do Pop"?',
     'Michael Jackson.'
+) 
+ 
+criaCartao(
+    'Atualidades',
+    'O que é a COP (Conferência das Partes) no contexto ambiental?',
+    'É uma conferência anual das Nações Unidas onde países discutem ações para combater as mudanças climáticas.'
 )
