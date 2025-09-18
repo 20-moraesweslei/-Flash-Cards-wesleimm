89624f0262950950913d4899ba029d3547de175e
20-moraesweslei <140934241+20-moraesweslei@users.noreply.github.com>
2025-09-18 05:19:15 +0000

    1

--- perguntas.js
+++ perguntas.js
@@ -1,47 +1,5 @@
 criaCartao(
-    'Conhecimento Geral',
-    'Qual é o maior país do mundo em extensão territorial?',
-    'Rússia'
-)
-
-criaCartao(
-    'Ciências',
-    'O que é a fotossíntese?',
-    'É o processo pelo qual as plantas produzem seu próprio alimento usando luz solar, água e dióxido de carbono, liberando oxigênio como subproduto.'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual é o rio mais longo do mundo?',
-    'O rio Nilo (embora o rio Amazonas dispute o título, dependendo do critério usado).'
-)
-
-criaCartao(
-    'Biologia',
-    'Quantos cromossomos tem o ser humano?',
-    '46 cromossomos (23 pares).'
-)
-
-criaCartao(
-    'Filme',
-    'Se você estivesse em um apocalipse zumbi, qual personagem famoso gostaria de ter ao seu lado?',
-    'Daryl Dixon (The Walking Dead) – o cara sabe sobreviver, é leal e tem uma besta!'
-)
-
-criaCartao(
-    'Literatura',
-    'Quem escreveu Dom Quixote?',
-    'Miguel de Cervantes.'
-)
-
-criaCartao(
-    'Cinema',
-    'Qual é o nome do personagem principal interpretado por Vin Diesel na franquia Velozes e Furiosos?',
-    'Dominic Toretto.'
-)
-
-criaCartao(
-    '...',
-    'E se você pudesse ter qualquer superpoder por um dia, qual escolheria?'
-    'Teletransporte – assim daria pra conhecer o mundo inteiro em 24 horas sem gastar nada!'
+    'Música',
+    'Quem é conhecido como o "Rei do Pop"?',
+    'Michael Jackson.'
 )
