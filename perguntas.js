0c9c41d86619b010ee5c00aeac2d45b9e38a4b47
20-moraesweslei <140934241+20-moraesweslei@users.noreply.github.com>
2025-09-18 05:12:46 +0000

    18

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,47 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Conhecimento Geral',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Rússia'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é a fotossíntese?',
+    'É o processo pelo qual as plantas produzem seu próprio alimento usando luz solar, água e dióxido de carbono, liberando oxigênio como subproduto.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o rio mais longo do mundo?',
+    'O rio Nilo (embora o rio Amazonas dispute o título, dependendo do critério usado).'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Biologia',
+    'Quantos cromossomos tem o ser humano?',
+    '46 cromossomos (23 pares).'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Filme',
+    'Se você estivesse em um apocalipse zumbi, qual personagem famoso gostaria de ter ao seu lado?',
+    'Daryl Dixon (The Walking Dead) – o cara sabe sobreviver, é leal e tem uma besta!'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu Dom Quixote?',
+    'Miguel de Cervantes.'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual é o nome do personagem principal interpretado por Vin Diesel na franquia Velozes e Furiosos?',
+    'Dominic Toretto.'
+)
+
+criaCartao(
+    '...',
+    'E se você pudesse ter qualquer superpoder por um dia, qual escolheria?'
+    'Teletransporte – assim daria pra conhecer o mundo inteiro em 24 horas sem gastar nada!'
 )
