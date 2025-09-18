c8aba82b6898c6b9c99f4ad189eb507361d3b46b
20-moraesweslei <140934241+20-moraesweslei@users.noreply.github.com>
2025-09-18 05:24:46 +0000

    1

--- perguntas.js
+++ perguntas.js
@@ -8,4 +8,10 @@ criaCartao(
     'Atualidades',
     'O que é a COP (Conferência das Partes) no contexto ambiental?',
     'É uma conferência anual das Nações Unidas onde países discutem ações para combater as mudanças climáticas.'
+) 
+
+criaCartao(
+    'Hipotética',
+    'E se você pudesse ter qualquer superpoder por um dia, qual escolheria?',
+    'Teletransporte – assim daria pra conhecer o mundo inteiro em 24 horas sem gastar nada!'
 )
